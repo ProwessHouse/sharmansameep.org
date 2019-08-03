d9495dd88514cadea979a7b47b09d4fca4e77405
Nilam Doctor <nilamdoc@gmail.com>
2019-08-03 21:17:22 +0530

    Initial

---
+++ app/config/bootstrap/connections.php
@@ -0,0 +1,28 @@
+<?php
+use lithium\data\Connections;
+
+ Connections::add('default', array(
+ 	'type' => CONNECTION_TYPE,
+ 	'host' => array(CONNECTION,
+		),
+//	'replicaSet' => true,
+ 	'database' => CONNECTION_DB,
+	'login' => CONNECTION_USER,
+	'password' => CONNECTION_PASS,	
+//	'setSlaveOkay' => true,
+//	'readPreference' => Mongo::RP_NEAREST	
+ ));
+/*
+ Connections::add('Distributor', array(
+ 	'type' => CONNECTIONDP_TYPE,
+ 	'host' => array(CONNECTIONDP,
+		),
+//	'replicaSet' => true,
+ 'database' => CONNECTIONDP_DB,
+	'login' => CONNECTIONDP_USER,
+	'password' => CONNECTIONDP_PASS,	
+//	'setSlaveOkay' => true,
+//	'readPreference' => Mongo::RP_NEAREST	
+ ));
+*/
+?>
